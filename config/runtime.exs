99f8143f3c97925ade1c52647c8f2bf9ca1a7f18
Zi Makki <zimakki@gmail.com>
2025-05-23 16:06:50 +0200

    make sure we have the maps key set

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,7 +3,11 @@ import Config
 google_places_api_key = System.get_env("GOOGLE_PLACES_API_KEY") ||
   raise "environment variable GOOGLE_PLACES_API_KEY is missing. Please set it before starting the application."
 
+google_maps_js_api_key = System.get_env("GOOGLE_MAPS_JS_API_KEY") ||
+  raise "environment variable GOOGLE_MAPS_JS_API_KEY is missing. Please set it before starting the application."
+
 config :delivery_map, google_maps_api_key: google_places_api_key
+config :delivery_map, google_maps_js_api_key: google_maps_js_api_key
 
 # config/runtime.exs is executed for all environments, including
 # during releases. It is executed after compilation and before the
